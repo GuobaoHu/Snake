62efd4a07655ebf7ef93854805526f22a3526f86
GuobaoHu <398456458@qq.com>
2017-12-13 17:02:38 +0800

    SNAKE0.2 gm窗口

--- src/guyue/hu/GM.java
+++ src/guyue/hu/GM.java
@@ -1,5 +1,43 @@
 package guyue.hu;
 
-public class GM {
+import java.awt.*;
+import java.awt.event.*;
 
+public class GM extends Frame {
+	public static final int GAME_WIDTH = 700;
+	public static final int GAME_HEIGHT = 600;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 500;
+	public static final int GAP = 50;
+	public static final int UNIT = 25;
+	private Rectangle Rect;
+	
+	public static void main(String[] args) {
+		GM gm = new GM();
+		gm.launch();
+	}
+	
+	public void launch() {
+		this.setBounds(300, 300, GAME_WIDTH, GAME_HEIGHT);
+		this.addWindowListener(new Close());
+		this.setVisible(true);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawRect(GAP, GAP, WIDTH, HEIGHT);
+		for(int i=1; i<HEIGHT/UNIT; i++) {
+			g.drawLine(GAP, GAP+UNIT*i, GAP+WIDTH, GAP+UNIT*i);
+		}
+		for(int i=0; i<WIDTH/UNIT; i++) {
+			g.drawLine(GAP+UNIT*i, GAP, GAP+UNIT*i, GAP+HEIGHT);
+		}
+	}
+	
+	private class Close extends WindowAdapter {
+		@Override
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
 }
